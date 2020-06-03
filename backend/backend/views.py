3b80785f0eadca70877bf0eaf6829960dbe45f4b
Aaron Leopold <aaronleopold1221@gmail.com>
2020-06-02 20:16:57 -0400

    basic styling to product info page

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -4,8 +4,32 @@ from django.shortcuts import render
 dummy_data = [
     {
         'name': 'Lemonade',
+        'current_real_price': 0,
+        'direct_labor_time': 0,
+        'direct_wages': 5,
+        'est_cost_price': 100,
+        'est_time_labor': 0,
+        'dependencies': [
+            ('water', 0),
+            ('ice', 0),
+            ('lemon', 0)
+        ]
+    },
+    {
+        'name': 'Mixtape',
+        'current_real_price': 0,
+        'direct_labor_time': 0,
+        'direct_wages': 2,
+        'est_cost_price': 10,
+        'est_time_labor': 0,
+        'dependencies': [
+            ('fire', 100000),
+            ('creativity', 10000),
+            ('pure skill', 1000)
+        ]
     }
 ]
+
 
 selected_product = dummy_data[0]
 
