009753754bdb504a03eae154cd56f9791c91e545
Aaron Leopold <aaronleopold1221@gmail.com>
2020-06-05 17:07:15 -0400

    edit product overview modal works and posts to DB

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -2,54 +2,9 @@ from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect
 from .models import Product
-from .forms import CreateProductForm
-
-
-# products = Product.objects.all()
-# proucts = Product.object.get(owner = logged in user)
-dummy_data = [
-    {
-        'name': 'Lemonade',
-        'current_real_price': 0,
-        'direct_labor_time': 0,
-        'direct_wages': 5,
-        'est_cost_price': 100,
-        'est_time_labor': 0,
-        'dependencies': [
-            ('water', 0),
-            ('ice', 0),
-            ('lemon', 0),
-            ('sugar', 0),
-        ]
-    },
-    {
-        'name': 'Mixtape',
-        'current_real_price': 0,
-        'direct_labor_time': 0,
-        'direct_wages': 2,
-        'est_cost_price': 10,
-        'est_time_labor': 0,
-        'dependencies': [
-            ('fire', 100000),
-            ('creativity', 10000),
-            ('pure skill', 1000)
-        ]
-    },
-        {
-        'name': 'Pakudex',
-        'current_real_price': 0,
-        'direct_labor_time': 0,
-        'direct_wages': 2,
-        'est_cost_price': 10,
-        'est_time_labor': 0,
-        'dependencies': [
-            ('developers', 5),
-        ]
-    }
-]
+from .forms import ProductForm
 
 # HELPERS
-
 # I think eventually we should change this
 # If a user creates a product called "This is my product,"
 # then the URL would be "/product/This is my product" which
@@ -61,38 +16,43 @@ def retrieve_product(product_name):
     except:    
         return None
 
+def handle_create_product(form):
+    name = form.cleaned_data['name']
+    real_price = form.cleaned_data['real_price']
+    direct_labor = form.cleaned_data['direct_labor']
+    direct_wages = form.cleaned_data['direct_wages']
+    indirect_wages = form.cleaned_data['indirect_wages']
+    indirect_labor = form.cleaned_data['indirect_labor']
+    product = Product(
+        name=name,
+        real_price=real_price,
+        direct_labor=direct_labor,
+        direct_wages=direct_wages,
+        indirect_wages=indirect_wages,
+        indirect_labor=indirect_labor
+    )
+
+    product.save()
+
+# def handle_edit_product(form, name):
+
+
 
 # VIEWS
-
 def fourohfour(request):
     return render(request, 'fourohfour/fourohfour.html')
 
+# I do not like the structure I have here, but since
+# there is only one POST operation it will be okay
+# FIXME: maybe? 
 def home(request):
     if request.method == 'POST':
-        form = CreateProductForm(request.POST)
+        form = ProductForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            real_price = form.cleaned_data['real_price']
-            direct_labor = form.cleaned_data['direct_labor']
-            direct_wages = form.cleaned_data['direct_wages']
-            indirect_wages = form.cleaned_data['indirect_wages']
-            indirect_labor = form.cleaned_data['indirect_labor']
-
-            new_product = Product(
-                name=name,
-                real_price=real_price,
-                direct_labor=direct_labor,
-                direct_wages=direct_wages,
-                indirect_wages=indirect_wages,
-                indirect_labor=indirect_labor
-            )
-
-            new_product.save()
-
-            
+            handle_create_product(form)
             return HttpResponseRedirect("")
     else:
-        form = CreateProductForm()
+        form = ProductForm()
 
     context = {
         'products': Product.objects.all(),
@@ -100,17 +60,40 @@ def home(request):
     }
     return render(request, 'home/index.html', context)
 
+
+# I do not like the structure I have here, and it will
+# break once I add the POST for adding / editing dependency values
+# FIXME: definitely
 def product_view(request, name):
     target_product = retrieve_product(name)
     
     if target_product is None:
         return redirect('/fourohfour')
     else:
+        if request.method == 'POST':
+            form = ProductForm(request.POST)
+            if form.is_valid():
+                Product.objects.filter(name=name).update(
+                    name = form.cleaned_data['name'],
+                    real_price = form.cleaned_data['real_price'],
+                    direct_labor = form.cleaned_data['direct_labor'],
+                    direct_wages = form.cleaned_data['direct_wages'],
+                    indirect_wages = form.cleaned_data['indirect_wages'],
+                    indirect_labor = form.cleaned_data['indirect_labor']
+                )
+                return HttpResponseRedirect("/product/{}".format(form.cleaned_data['name']))
+            else:
+                print(form._errors)
+
+        else:
+            form = ProductForm()
         context = {
             'product': target_product,
-            'dependencies': []
+            'dependencies': [],
+            'form': form
         }
         return render(request, 'home/product_info.html', context)
+
 
 def product_analytics(request, name):
     target_product = retrieve_product(name)
@@ -123,6 +106,7 @@ def product_analytics(request, name):
             'dependencies': []
         }
         return render(request, 'home/product_analytics.html', context)
+
 
 def create_product(request):
     return render(request, 'home/test.html', {'req': request})
