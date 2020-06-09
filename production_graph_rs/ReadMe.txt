ddfbd921f53c8f5572761d4d4805d97221c6a393
Cameron R <cameron.g.rodgers@gmail.com>
2020-06-09 09:38:32 -0400

    more detailed benchmark, and a readme

---
+++ production_graph_rs/ReadMe.txt
@@ -0,0 +1,10 @@
+To run this code:
+- install the rust toolchain (rustup, cargo, rustc)
+
+Run these commands:
+	To test the code:
+	cargo test
+
+	To run the function in main.rs (note: may run a heavy benchmark):
+	cargo run --release
+
--- production_graph_rs/src/main.rs
+++ production_graph_rs/src/main.rs
@@ -6,8 +6,6 @@ use product_graph_rayon::*;
 use std::time::Instant;
 
 fn main() {
-    let num_prods = 1_000_000;
-    let mut data = ProductGraph::generate_product_graph(num_prods);
     //    let mut data = ProductGraph::with_capacity(4);
     //    data.insert(0,Product{direct_cost: 5.0, indirect_cost: 0.0, dependencies: vec![DependencyInfo{ id: 1, quantity:  1.01}]});
     //    data.insert(1,Product{direct_cost: 10.0, indirect_cost: 0.0, dependencies: vec![DependencyInfo{ id: 0, quantity:  1.9}]});
@@ -20,18 +18,30 @@ fn main() {
     //        println!("err");
     //    }
     //}
+    benchmark();
 
-    let num_iters = 50;
-    let start = Instant::now();
-    let _results = data.calc_for_n_iterations(num_iters);
-    let duration = start.elapsed();
-
-    println!(
-        "Time elapsed in calc_for_n_iterations for {} iterations on {} products is: {:?}",
-        num_iters, num_prods, duration
-    );
 
     //for (id, c) in data.iter().enumerate() {
     //    println!("id: {}, direct_cost: {}, dep_count: {}, indirect_cost: {}", id, c.direct_cost, c.dependencies.len(), c.indirect_cost);
     //}
+}
+
+
+fn benchmark() {
+    let num_iters = 25;
+    let mut times = Vec::new();
+    for i in 0..15 {
+        let num_prods = 1_000_000 * i;
+        let data = ProductGraph::generate_product_graph(num_prods);
+        let start = Instant::now();
+        let _results = data.calc_for_n_iterations(num_iters);
+        let duration = start.elapsed();
+        times.push((i, duration));
+    }
+
+    for (multiple, duration) in times {
+        println!("Time elapsed for {} iterations on {} * 1mil products is: {:?}",
+                 num_iters, multiple, duration
+                 );
+    }
 }
