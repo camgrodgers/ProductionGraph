99d98a984b13e28420320b0377edede185312656
Aaron Leopold <aaronleopold1221@gmail.com>
2020-06-25 15:20:43 -0400

    added docstrings for finished functions in views.py and api.py

--- backend/backend/api.py
+++ backend/backend/api.py
@@ -7,6 +7,16 @@ from .forms import ProductForm
 ### CRUD FOR PRODUCT ###
 
 def create_product(request):
+    """
+    Creates a product in the database. This function handles POST requests sent to the URL 'api/create/product'
+
+    :param request: The request sent to server
+    :type request: HttpRequest
+
+    :return: a redirect to the homepage '/' on successful save to DB, to '/fourohfour' on failure or on request 
+        types that are not POST
+    :rtype: HttpResponseRedirect
+    """
     # handle the post to this url ONLY
     if request.method == 'POST':
         form = ProductForm(request.POST)
@@ -30,6 +40,19 @@ def create_product(request):
 
 # TODO: add safety try/except blocks (see delete_product)
 def edit_product(request, name):
+    """
+    Updates a product in the database. This function handles POST requests sent to the URL 'api/edit/product/:id'
+
+    :param request: The request sent to server
+    :type request: HttpRequest
+
+    :param name: the name of the product selected to update
+    :type name: str (This will change to int to reflect the future change in the Product model)
+
+    :return: a redirect to the selected product info page using new id '/product/:id' on successful find and update to DB, 
+        to '/product/:id' using original id on failure and to '/fourohfour' on request types that are not POST
+    :rtype: HttpResponseRedirect
+    """
     # url should only accept post requests
     if request.method == 'POST':
         form = ProductForm(request.POST)
@@ -57,6 +80,19 @@ def edit_product(request, name):
 
 
 def delete_product(request, name):
+    """
+    Deletes a product in the database. This function handles POST requests sent to the URL 'api/delete/product/:id'
+
+    :param request: The request sent to server
+    :type request: HttpRequest
+
+    :param name: the name of the product selected to delete
+    :type name: str (This will change to int to reflect the future change in the Product model)
+
+    :return: a redirect to the homepage '/' on successful find and delete to DB, to '/fourohfour' on failure or on request 
+        types that are not POST
+    :rtype: HttpResponseRedirect
+    """
     # TODO: change to DELETE request??
     if request.method == 'POST':
         form = ProductForm(request.POST)
@@ -82,12 +118,18 @@ def delete_product(request, name):
 ### CRUD FOR DEPENDENCY ###
 
 def create_dependency(request):
+    """
+    """
     pass
 
 
 def edit_dependency(request, prod_name):
+    """
+    """
     pass
 
 
 def delete_dependency(request, dep_name):
+    """
+    """
     pass
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -11,6 +11,15 @@ from .forms import ProductForm
 # is very gross. Perhaps add a __str__ function to the Model
 # that returns all lowercase string of first word in name?
 def retrieve_product(product_name):
+    """
+    Queries DB for a product by name (This will be updated to query by ID)
+
+    :param product_name: The name of target product (This will be updated to query by ID)
+    :type request: str
+
+    :return: a Product object on success, None on failure
+    :rtype: Product or None
+    """
     try:
         return Product.objects.get(name=product_name)
     except:    
@@ -23,10 +32,33 @@ def retrieve_product(product_name):
 ### VIEWS ###
 
 def fourohfour(request):
+    """
+    GET request handler for the URL '/fourohfour'
+
+    :param request: The request sent to server
+    :type request: HttpRequest
+
+    :return: HttpResponse containing the page HTML, an HttpResponseRedirect to the 404 page '/fourohfour' if 
+        request type isn't a GET
+    :rtype: HttpResponse or HttpResponseRedirect
+    """
+    if request.method != 'GET':
+        return HttpResponseRedirect("/fourohfour")
+
     return render(request, 'fourohfour/fourohfour.html')
 
 
 def home(request):
+    """
+    GET request handler for the URL '/'
+
+    :param request: The request sent to server
+    :type request: HttpRequest
+    
+    :return: HttpResponse containing the page HTML and a context of a list of Product objects, and an
+         HttpResponseRedirect to the 404 page '/fourohfour' if request type isn't a GET
+    :rtype: HttpResponse or HttpResponseRedirect
+    """
     if request.method != 'GET':
         return HttpResponseRedirect("/fourohfour")
 
@@ -42,6 +74,19 @@ def home(request):
 # 2). Add Button + Confirmation modal (i.e. "Are you sure?") for delete product
 
 def product_view(request, name):
+    """
+    GET request handler for the URL '/product/:id'
+
+    :param request: The request sent to server
+    :type request: HttpRequest
+
+    :param name: the name of the product selected
+    :type name: str (This will change to int to reflect the future change in the Product model)
+
+    :return: HttpResponse containing the page HTML and a context of the selected Product object, and an
+         HttpResponseRedirect to the 404 page '/fourohfour' if request type isn't a GET
+    :rtype: HttpResponse or HttpResponseRedirect
+    """
     target_product = retrieve_product(name)
     
     if target_product is None or request.method != 'GET':
@@ -55,6 +100,19 @@ def product_view(request, name):
 
 
 def product_analytics(request, name):
+    """
+    GET request handler for the URL '/product/:id/analytics/'
+
+    :param request: The request sent to server
+    :type request: HttpRequest
+
+    :param name: the name of the product selected
+    :type name: str (This will change to int to reflect the future change in the Product model)
+
+    :return: HttpResponse containing the page HTML and a context of the selected Product object, and an
+         HttpResponseRedirect to the 404 page '/fourohfour' if request type isn't a GET
+    :rtype: HttpResponse or HttpResponseRedirect
+    """
     target_product = retrieve_product(name)
 
     if target_product is None or request.method != "GET":
@@ -65,7 +123,3 @@ def product_analytics(request, name):
             'dependencies': []
         }
         return render(request, 'home/product_analytics.html', context)
-
-
-def create_product(request):
-    return render(request, 'home/test.html', {'req': request})
