f58ca0f94628aabb9851bc8466c7948428616ce7
Cameron R <cameron.g.rodgers@gmail.com>
2020-06-30 20:12:23 -0400

    got everything working on pip

--- package/backend/settings.py
+++ package/backend/settings.py
@@ -122,4 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static_built/'
+STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, "backend/static_built")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "backend/static_built")]
--- package/setup.py
+++ package/setup.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import setuptools
     setuptools.setup(
         name='productiongraph',
-        version='0.1.5',
+        version='0.1.6',
         author='Cameron, Aaron, Osiris, Benjamin, Matt',
         author_email='cameron.g.rodgers@gmail.com',
         packages=setuptools.find_packages(),
